c53a89f2f77825eb9790e3b3a938253a6bbeed7c
The Gentleman <GentlemenSuite@proton.me>
2025-10-12 20:09:41 +0200

    restructure ...

--- nakama/GentleRotations/GentleSpellsGeneric.lua
+++
@@ -1,8 +0,0 @@
-local nakamaMedia, _A, nakama = ...
-
--- initialize tables
-nakama.SpellBook = nakama.SpellBook or {}
-nakama.SpellBook.Generic = {}
-
--- Ranged Generic Spells
-nakama.SpellBook.Generic.Throw = _A.GetSpellInfo(2764)
--- rotations/rogue/GentleRogue_Leveling.lua
+++ rotations/rogue/GentleRogue_Leveling.lua
@@ -1,8 +1,18 @@
 local nakamaMedia, _A, nakama = ...
-local player, target, targetGUID
+local player, target, targetGUID, facing, spell
 local playerGUID = _A.Cache.Utils.playerGUID or _A.UnitGUID("player")
-local apepDir = _A.GetApepDirectory()
-_A.require(apepDir .. "\\nakama\\GentleRotations\\SpellBook\\GentleRogue_SpellBook.lua")
+-- empty pp blacklist table for initalization
+local pickPocketBlackList = {}
+-- initalize lastPP target
+local lastPickPocketTarget = nil
+
+-- spell block
+--> cp builder
+local sinisterStrike = _A.GetSpellInfo(1101752)
+--> finisher
+local eviscerate = _A.GetSpellInfo(1102098)
+--> generic
+local throw = _A.GetSpellInfo(2764)
 
 -- to do: gui settings and modifiers
 local gui = {}
@@ -24,11 +34,14 @@ local function inCombat()
 
     -- reset loop if casting (slam or item for example)
     if player:IscastingAnySpell()
-        -- reset loop if stunned or silenced
-        -- racial implementation for dispel soon
-        or player:State("stun || silence")
         -- reset loop if mounted
         or player:Mounted() then
+        return true
+    end
+
+    -- reset loop if stunned or silenced
+    -- racial implementation for dispel soon
+    if player:State("stun || silence") then
         return true
     end
 
@@ -37,6 +50,24 @@ local function inCombat()
     -- check if we have a target thats not dead or a friend
     if target then
         targetGUID = target.guid
+
+        if target:Alive() and target:Enemy() then
+            facing = _A.UnitIsFacing(playerGUID, targetGUID, 130)
+
+            if player:SpellReady(sinisterStrike)
+                and player:Combo() < 5
+                and target:SpellRange(sinisterStrike)
+                and facing then
+                return target:Cast(sinisterStrike)
+            end
+
+            if player:SpellReady(eviscerate)
+                and player:Combo() == 5
+                and target:SpellRange(sinisterStrike)
+                and facing then
+                return target:Cast(eviscerate)
+            end
+        end
     end
 end
 
