2bcf4c6ea358ebdc3b8d5dfa478c6d87e090e103
TheGentleman <GentlemenSuite@proton.me>
2025-10-12 14:00:48 +0200

    Paladin Cleanup
    - renamed files and updated some  stuff

---
+++ rotations/paladin/paladin_leveling.lua
@@ -0,0 +1,218 @@
+local nakamaMedia, _A, nakama = ...
+local player, target, roster, inRange, facing, inLOS
+local crusaderStrike = _A.GetSpellInfo(1135395)
+local judgmentOfLight = _A.GetSpellInfo(1120271)
+local devotionAura = _A.GetSpellInfo(1100465)
+local holyLight = _A.GetSpellInfo(1100635)
+local blessingOfMight = _A.GetSpellInfo(1119740)
+local sealOfRighteousness = _A.GetSpellInfo(1121084)
+local drink = _A.GetSpellInfo(430)
+local eat = _A.GetSpellInfo(433)
+
+local function playerCantCast()
+    return player:State("stun || silence")
+end
+local function invalidTarget()
+    return not target or target and (target:Friend() or target:Dead())
+end
+local function invalidSpell(spell)
+    return not player:SpellReady(spell) or player:IscastingAnySpell()
+end
+local function isValidMeleeTargetCast(spell)
+    local fcing = _A.UnitIsFacing(player.guid, target.guid, 130)
+end
+
+local gui = {}
+
+local exeOnLoad = function()
+    _A.UIErrorsFrame:Hide()
+    _A.Sound_EnableErrorSpeech = 0
+end
+
+local exeOnUnload = function() end
+
+local inCombat = function()
+    player = _A.Object("player")
+
+    if not player then
+        return
+    end
+
+    target = _A.Object("target")
+
+    -- cancel if casting
+    if player:IscastingAnySpell()
+        -- cancel if stunned or silenced
+        -- racial implementation for dispel soon
+        or player:State("stun || silence")
+        -- cancel if mounted
+        or player:Mounted() then
+        -- reset loop
+        return
+    end
+
+    -- check if we can cast Devotion Aura
+    if player:SpellReady(devotionAura)
+        -- cancel if we have buff already
+        and not player:Buff(devotionAura) then
+        -- cast Devotion Aura
+        return player:Cast(devotionAura)
+    end
+
+        -- check if we can cast Divine Protection
+    if player:SpellReady(holyLight)
+        -- cancel if moving
+        and not player:Moving()
+        -- cancel if hp % > 40
+        and player:Health() < 40 then
+        -- cast Holy Light on player
+        return player:Cast(holyLight)
+    end
+
+    -- check if we can cast Holy Light
+    if player:SpellReady(holyLight)
+        -- cancel if moving
+        and not player:Moving()
+        -- cancel if hp % > 40
+        and player:Health() < 40 then
+        -- cast Holy Light on player
+        return player:Cast(holyLight)
+    end
+
+    -- check if we can cast Seal of Righteousness
+    if player:SpellReady(sealOfRighteousness)
+        -- cancel if we have buff already
+        and not player:Buff(sealOfRighteousness) then
+        -- cast Seal of Righteousness on player
+        return player:Cast(sealOfRighteousness)
+    end
+
+    -- check if we have a target thats not dead or a friend
+    if target and not (target:Dead() or target:Friend()) then
+        -- check if we can cast Judgement of Light
+        if player:SpellReady(judgmentOfLight)
+            -- cancel if target not in range
+            and target:SpellRange(judgmentOfLight)
+            -- cancel if target not in los
+            and target:Los() then
+            -- cast Judgement of Light on target
+            return target:Cast(judgmentOfLight)
+        end
+
+        -- check if we can cast Crusader Strike
+        if player:SpellReady(crusaderStrike)
+            -- cancel if target not in range
+            and target:SpellRange(crusaderStrike)
+            -- cancel if target not in 130° cone in front
+            and _A.UnitIsFacing(player.guid, target.guid, 130) then
+            -- cast Crusader Strike on target
+            return target:Cast(crusaderStrike)
+        end
+    end
+end
+
+
+
+
+local outCombat = function()
+    player = _A.Object("player")
+
+    if not player then
+        return
+    end
+
+    -- cancel if casting
+    if player:IscastingAnySpell()
+        -- cancel if stunned or silenced
+        -- racial implementation for dispel soon
+        or player:State("stun || silence")
+        -- cancel if mounted
+        or player:Mounted()
+        -- cancel if eating or drinking
+        or (player:BuffAny(drink) or player:BuffAny(eat)) then
+        -- reset loop
+        return
+    end
+
+    -- check if we can cast Devotion Aura
+    if player:SpellReady(devotionAura)
+        -- cancel if casting
+        and not player:IscastingAnySpell()
+        -- cancel if we have buff already
+        and not player:Buff(devotionAura) then
+        -- cast Devotion Aura
+        return player:Cast(devotionAura)
+    end
+
+    -- check if we can cast Holy Light
+    if player:SpellReady(holyLight)
+        -- cancel if casting
+        and not player:IscastingAnySpell()
+        -- cancel if moving
+        and not player:Moving()
+        -- cancel if hp % > 40
+        and player:Health() < 40 then
+        -- cast Holy Light on player
+        return player:Cast(holyLight)
+    end
+
+    -- check if we can cast Seal of Righteousness
+    if player:SpellReady(sealOfRighteousness)
+        -- cancel if casting
+        and not player:IscastingAnySpell()
+        -- cancel if we have buff already
+        and not player:Buff(sealOfRighteousness) then
+        -- cast Seal of Righteousness on player
+        return player:Cast(sealOfRighteousness)
+    end
+
+    -- check if we can cast Blessing of Might
+    if player:SpellReady(blessingOfMight) then
+        -- if we are in group
+        if player:Ingroup() then
+            -- fetch roster
+            roster = _A.OM:Get("Roster")
+
+            -- for each mate in our roster
+            for _, mate in pairs(roster) do
+                -- cancel if mate has any Blessing of Might buff
+                if not mate:BuffAny(blessingOfMight)
+                    -- cancel if not in spellrange
+                    and mate:SpellRange(blessingOfMight)
+                    -- cancel if not los
+                    and mate:Los() then
+                    -- cast Blessing of Might on mate
+                    return mate:Cast(blessingOfMight)
+                end
+            end
+        else
+            -- cancel if we have buff already
+            if not player:BuffAny(blessingOfMight) then
+                -- cast Blessing of Might on player
+                return player:Cast(blessingOfMight)
+            end
+        end
+    end
+end
+
+
+
+local spellIds_Loc = {}
+
+local blacklist = {}
+
+_A.CR:Add("Paladin", {
+    name = "Nakama - Leveling (PVE)",
+    ic = inCombat,
+    ooc = outCombat,
+    use_lua_engine = true,
+    gui = gui,
+    gui_st = { title = "CR Settings", color = "87CEFA", width = "315", height = "370" },
+    wow_ver = "3.3.5",
+    apep_ver = "1.1",
+    -- ids = spellIds_Loc,
+    -- blacklist = blacklist,
+    -- pooling = false,
+    load = exeOnLoad,
+    unload = exeOnUnload,
+})
