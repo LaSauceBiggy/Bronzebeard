8314c9605a1ec45a19a846f296dfa84447bfabea
The Gentleman <GentlemenSuite@proton.me>
2025-10-14 20:20:19 +0200

    Create nakamaLoot.lua
    - return to nakama namespace to make it universally accessible
    - authors may still create their own namespace instead!

---
+++ nakama/modules/nakamaLoot.lua
@@ -0,0 +1,53 @@
+--------------------------------------------------------------------------------
+-- nakamaLoot - Module
+-- Author: nakamaman
+--------------------------------------------------------------------------------
+local nakama, _A, nakama = ...
+
+-- Local blacklist cache (O(1) lookups)
+nakama.lootBlackList = {}
+
+--------------------------------------------------------------------------------
+-- Listener: reset blacklist when entering combat
+--------------------------------------------------------------------------------
+function nakama.addLootListener()
+    _A.Listener:Add("nakamaLoot", { "PLAYER_REGEN_DISABLED" }, function(event)
+        if event == "PLAYER_REGEN_DISABLED" then
+            for k in pairs(nakama.lootBlackList) do
+                nakama.lootBlackList[k] = nil
+            end
+        end
+    end)
+end
+
+--------------------------------------------------------------------------------
+-- Listener removal
+--------------------------------------------------------------------------------
+function nakama.deleteLootListener()
+    _A.Listener:Remove("nakamaLoot")
+end
+
+--------------------------------------------------------------------------------
+-- Auto-loot routine
+-- Runs O(n) over visible corpses
+--------------------------------------------------------------------------------
+function nakama.autoLoot()
+    if _A.BagSpace() < 1 then return false end
+
+    local corpses = _A.OM:Get("Dead")
+    if not corpses then return false end
+
+    local blacklist = nakama.lootBlackList
+
+    for _, corpse in pairs(corpses) do
+        if corpse:Hasloot() and corpse:Distance() < 4.5 then
+            local guid = corpse.guid
+            if not blacklist[guid] then
+                _A.InteractUnit(guid)
+                blacklist[guid] = true
+                _A.ClearTarget()
+                return true
+            end
+        end
+    end
+end
